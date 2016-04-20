9102ab946a86f149d3d4412a6f351a13104c1b3e
Jonathan <panagiotidisjonathan@gmail.com>
2016-04-20 17:21:16 +0300

    added async await

--- source/js/app.js
+++ source/js/app.js
@@ -1,5 +1,17 @@
 'use strict';
 
-require('../sass/lp.scss');
+import '../sass/lp.scss';
 
 console.log('YEAH');
+
+moufa()
+.then((data) => {
+  console.log('YEAH', data);
+})
+.catch((err) => {
+  console.log('ERR', err);
+});
+
+async function moufa(){
+  console.log('FFFF');
+}
