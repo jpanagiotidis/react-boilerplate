2c3fe40e2a12fb888c8e28e2692f0aa2add4e8f2
Jonathan <panagiotidisjonathan@gmail.com>
2016-04-21 11:05:40 +0300

    [server] added index.html send on all not found routes

---
+++ server-polyfill.js
@@ -0,0 +1,10 @@
+require("babel-polyfill");
+require("babel-core/register")({
+  extensions: [".es6", ".es", ".jsx", ".js"],
+  presets: ["es2015", "stage-0"]
+});
+
+console.log(process.argv);
+const codePath  = process.argv[2] ? process.argv[2] : "app.js";
+
+require(`./source/server/js/${codePath}`);
