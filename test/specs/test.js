aa76293fceb948209579f3e0af83b65225e90eea
Christos Alatzidis <alatzidhs@gmail.com>
2016-04-21 16:37:30 +0300

    tests with selenium via webdriver

--- test/specs/test.js
+++ test/specs/test.js
@@ -9,12 +9,18 @@ describe('landing page', function() {
         assert.equal(txt, 'APP TITLE PAGE');
     });
 
-    it('should navigate after click to menu', function () {
+    it('should navigate on click to the proper pages', function () {
       browser.url('http://localhost:3000');
       browser.click('a');
       var txt = browser.getText('h1*=DASHBOARD');
       assert.equal(txt, 'DASHBOARD');
       var txt2 = browser.getText('h1*=HOME');
       assert.equal(txt2, 'HOME VIEW');
+      browser.click('a*=Users');
+      var txt3 = browser.getText('h1*=USERS');
+      assert.equal(txt3, 'USERS VIEW');
+      browser.click('a*=Settings');
+      var txt3 = browser.getText('h1*=SETTINGS');
+      assert.equal(txt3, 'SETTINGS VIEW');
     });
 });
